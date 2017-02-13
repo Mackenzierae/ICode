2c4e8f6e15ca4311143f4d6bfc1e6dc898b0bf86
Mackenzie Wacker <wacker.mackenzierae@gmail.com>
2017-02-13 11:30:26 -0800

    modified factory- replaced for-loops in conditionals with concat function, and changed URL to included param 'per_page=100'

--- client/factories/mainFactory.js
+++ client/factories/mainFactory.js
@@ -7,31 +7,30 @@ app.factory('mainFactory', ['$http', '$location', function($http, $location){
   var closedNumber = 1;
   var openNumber = 1;
 
+  //Call for CLOSED BUGG ISSUES
   factory.getClosedBugsData = function(callback){
+      //function(pagenumber, callback);
       $http({
         method: "get",
-        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=closed&page="+ closedNumber,
-        // url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&per_page=100",
+        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=closed&per_page=100&page="+ closedNumber,
     })
     .then(function(res){
-      console.log("in the response of the getData factory function");
-      console.log(res.data.length);
-
+      console.log("res of closed bugggggzzzzzzzzz: length = ", res.data.length);
+      // console.log("res.headers???:", res.headers('link'));
+      ////////////////////////////////////////
+      ////dealing with Pagination:
+      ////////////////////////////////////////
       if (res.data.length == 0) {
         closedNumber = 1;
         callback(closedData);
       }
-      else if(res.data.length > 0 && res.data.length < 30){
-        for (var i = 0; i < res.data.length; i++){
-          closedData.push(res.data[i]);
-        }
+      else if(res.data.length > 0 && res.data.length < 100){
+        closedData = closedData.concat(res.data);
         closedNumber = 1;
         callback(closedData)
       }
       else {
-        for (var i = 0; i < res.data.length; i++){
-          closedData.push(res.data[i]);
-        }
+        closedData = closedData.concat(res.data);
         closedNumber++;
         factory.getClosedBugsData(callback);
       }
@@ -41,35 +40,31 @@ app.factory('mainFactory', ['$http', '$location', function($http, $location){
   };
 
 
+  //Call for OPEN BUGG ISSUES
   factory.getOpenBugsData = function(callback){
       $http({
         method: "get",
-        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&page="+ openNumber,
-        // url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&per_page=100",
+        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&per_page=100&page="+ openNumber,
     })
     .then(function(res){
-      console.log("in the response of the getData factory function");
-      console.log(res.data.length);
-
+      console.log("in the response of the getData factory function OPNENNNNNNNNNN:", res.data.length);
+      ////////////////////////////////////////
+      ////dealing with Pagination:
+      ////////////////////////////////////////
       if (res.data.length == 0) {
         openNumber = 1;
         callback(openData);
       }
-      else if(res.data.length > 0 && res.data.length < 30){
-        for (var i = 0; i < res.data.length; i++){
-          openData.push(res.data[i]);
-        }
+      else if(res.data.length > 0 && res.data.length < 100){
+        openData = openData.concat(res.data);
         openNumber = 1;
         callback(openData)
       }
       else {
-        for (var i = 0; i < res.data.length; i++){
-          openData.push(res.data[i]);
-        }
+        openData = openData.concat(res.data);
         openNumber++;
         factory.getOpenBugsData(callback);
       }
-
     }).catch(function(err){
       console.log(err);
     })
