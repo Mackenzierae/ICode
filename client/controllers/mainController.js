0d796af37b90dbc153374bd1150a9f97e5ec5790
Mackenzie Wacker <wacker.mackenzierae@gmail.com>
2017-02-18 14:26:07 -0800

    combined two factory functions into 1, changed 'state' param to 'all', temporarily ignoring pagination. Calling first 100 issues. Combined controller functions into one. got rid of 'since' param. Grabbing everything. Sorting all data into closedData and OpenData objects by week.

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -5,12 +5,8 @@ app.controller('mainController', ["$scope", "mainFactory", function($scope, main
 
   var closedData = {};
   var openData = {};
+  var checkForRandom = {};
 
-  //Easily change how many weeks we want to look at by changing the varible 'weeksBackWeWantToLookAt'
-  //If we go past 52 we'd have to break down by year which the code doesn't currently support.
-  //This can only apply to "closed" bug issues because Github API's 'since' param only looks at the "updated" time stamp.
-    //We're assuming here that the closing of the issue would be the last it's updated...
-    //but as you may notice we've got a random "week 41" straggler defying this system.
   var weeksBackWeWantToLookAt = 11
   var millisecondsInOneWeek = 604800000;
   var currentTimeStamp = new Date();
@@ -18,62 +14,67 @@ app.controller('mainController', ["$scope", "mainFactory", function($scope, main
   var blankWeeksAgoUnix = new Date(currentDateUnix - (millisecondsInOneWeek * weeksBackWeWantToLookAt))
   var closedSinceBlankWeeksAgoParam = blankWeeksAgoUnix.toISOString();
 
-//FUNCTION TO GET DATA ABOUT CLOSED BUGG ISSUES:
-  var getClosedBugsData = function(){
-    mainFactory.getClosedBugsData(closedSinceBlankWeeksAgoParam, function(data){
-      console.log("ALL BUG ISSUES CLOSED WITHIN THE PAST " + weeksBackWeWantToLookAt + " WEEKS.....:", data.length, data);
+//FUNCTION TO GET DATA ABOUT ALL BUGG ISSUES:
+  var getBugData = function(){
+    mainFactory.getBugData(function(data){
+      console.log(data.length, data);
 
       for (var i = 0; i < data.length; i++){
-        ///////...Average time to close bugs...//////////////
-              // var createdAt = Date.parse(data[i].created_at);
-              // var closedAt = Date.parse(data[i].closed_at);
-              // var difference = closedAt - createdAt;
-              // var oneDay = 86400;
-              // var oneHour = 3600;
-              // var days = (difference / 1000) / oneDay;
-              // // closedData[week].push(data[i], "number of days from create to close..." + days);
-        //////////////////////////////////////////////////
-        var date = data[i].closed_at;
-        var week = moment(date).week();
-          if (closedData[week]) {
-            closedData[week].push(data[i]);
-          }
-          else {
-            closedData[week] = [data[i]];
-          }
-        }
-        console.log("ALL CLOSED BUG ISSUES BROKEN DOWN WEEK BY WEEK......:", closedData);
-      })
-  }
-
-
-
-
-//FUNCTION TO GET ALL DATA ABOUT OPEN BUGG ISSUES:
-  var getOpenBugsData = function(){
-    mainFactory.getOpenBugsData(function(data){
-      console.log("DATA FOR ALL BUGS THAT ARE OPEN.......", data.length, data);
-      for (var i = 0; i < data.length; i++){
+        // console.log(data[i].state);
         var date = data[i].created_at;
         var week = moment(date).week();
         var year = moment(date).year();
-        if (openData[year]) {
-            if (openData[year][week]) {
-              openData[year][week].push(data[i]);
-            }
-            else {
-              openData[year][week] = [data[i]];
+
+        if (data[i].state == 'closed'){
+          if (closedData[year]) {
+              if (closedData[year][week]) {
+                closedData[year][week].push(data[i]);
               }
-            }
-        else {
-          openData[year] = {};
-          openData[year][week] = [data[i]];
+              else {
+                closedData[year][week] = [data[i]];
+                }
+              }
+          else {
+            closedData[year] = {};
+            closedData[year][week] = [data[i]];
+          }
         }
-      }
-    })
-    console.log("ALL OPEN BUG ISSUES BROKEN DOWN WEEK BY WEEK......:", openData);
+        else if (data[i].state == 'open'){
+          if (openData[year]) {
+              if (openData[year][week]) {
+                openData[year][week].push(data[i]);
+              }
+              else {
+                openData[year][week] = [data[i]];
+                }
+              }
+          else {
+            openData[year] = {};
+            openData[year][week] = [data[i]];
+          }
+        }
+        else {
+          checkForRandom.push(data[i]);
+        }
   }
 
-  getClosedBugsData();
-  getOpenBugsData();
+      console.log("CLOSED DATA.....", closedData);
+      console.log("OPEN DATA.....", openData);
+      // console.log("RANDOM.....", checkForRandom);
+    })
+  }
+
+
+  getBugData();
 }])
+
+
+///////...Average time to close bugs...//////////////
+      // var createdAt = Date.parse(data[i].created_at);
+      // var closedAt = Date.parse(data[i].closed_at);
+      // var difference = closedAt - createdAt;
+      // var oneDay = 86400;
+      // var oneHour = 3600;
+      // var days = (difference / 1000) / oneDay;
+      // // closedData[week].push(data[i], "number of days from create to close..." + days);
+//////////////////////////////////////////////////
--- client/factories/mainFactory.js
+++ client/factories/mainFactory.js
@@ -2,74 +2,42 @@ console.log("main factory");
 app.factory('mainFactory', ['$http', '$location', function($http, $location){
   var factory = {}
 
-  var closedData = [];
-  var openData = [];
-  var closedNumber = 1;
-  var openNumber = 1;
+  var data = [];
+  var pageCount = 1;
 
   //Call for CLOSED BUGG ISSUES
-  factory.getClosedBugsData = function(sinceParam, callback){
+  factory.getBugData = function(callback){
       $http({
         method: "get",
-        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=closed&per_page=100&page="+ closedNumber + "&since="+ sinceParam,
+        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=all&per_page=100&page=1",
+//       url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&per_page=100&page="+ openNumber,
     })
     .then(function(res){
+      console.log("got it");
+      callback(res.data);
       ////////////////////////////////////////
       ////dealing with Pagination:
       ////////////////////////////////////////
-      if (res.data.length == 0) {
-        closedNumber = 1;
-        callback(closedData);
-      }
-      else if(res.data.length > 0 && res.data.length < 100){
-        closedData = closedData.concat(res.data);
-        closedNumber = 1;
-        callback(closedData)
-      }
-      else {
-        closedData = closedData.concat(res.data);
-        closedNumber++;
-        factory.getClosedBugsData(callback);
-      }
+
+      // if (res.data.length == 0) {
+      //   closedNumber = 1;
+      //   callback(closedData);
+      // }
+      // else if(res.data.length > 0 && res.data.length < 100){
+      //   closedData = closedData.concat(res.data);
+      //   closedNumber = 1;
+      //   callback(closedData)
+      // }
+      // else {
+      //   closedData = closedData.concat(res.data);
+      //   closedNumber++;
+      //   factory.getClosedBugsData(callback);
+      // }
+
     }).catch(function(err){
       console.log(err);
     })
   };
-
-
-
-
-  //Call for ALL OPEN BUGG ISSUES
-  factory.getOpenBugsData = function(callback){
-      $http({
-        method: "get",
-        url: "https://api.github.com/repos/babel/babel/issues?labels=bug&state=open&per_page=100&page="+ openNumber,
-    })
-    .then(function(res){
-      ////////////////////////////////////////
-      ////dealing with Pagination:
-      ////////////////////////////////////////
-      if (res.data.length == 0) {
-        openNumber = 1;
-        callback(openData);
-      }
-      else if(res.data.length > 0 && res.data.length < 100){
-        openData = openData.concat(res.data);
-        openNumber = 1;
-        callback(openData)
-      }
-      else {
-        openData = openData.concat(res.data);
-        openNumber++;
-        factory.getOpenBugsData(callback);
-      }
-    }).catch(function(err){
-      console.log(err);
-    })
-  }
-
-
-
 
   return factory;
 }])
